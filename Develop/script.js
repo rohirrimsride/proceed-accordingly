c438dc329d9f0c6aea147192a0e39c236faacb7c
Strider <dave.barnes23@gmail.com>
2022-05-30 15:56:46 -0500

    completed bulk of styling (forgot to create feature)

---
+++ Develop/script.js
@@ -0,0 +1,5 @@
+// save entered data to local storage to persist
+// create a "clock" to track time for past, present and future appointments
+
+// assign classes to textarea based on current time
+
