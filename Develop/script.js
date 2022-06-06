c689b88cce42fb5d1094b6fc84e7d693f58ae53c
Strider <dave.barnes23@gmail.com>
2022-06-05 19:41:07 -0500

    added local storage and retrieval

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,24 @@
-
-var tasks = [];
-
 var time = document.getElementsByClassName(".time");
-var hourlyTasks = document.getElementById("9");
 
-// console.log(time);
+var saveBtn9 = document.getElementById("saveBtn9");
+var saveBtn10 = document.getElementById("saveBtn10");
+var saveBtn11 = document.getElementById("saveBtn11");
+var saveBtn12 = document.getElementById("saveBtn12");
+var saveBtn13 = document.getElementById("saveBtn13");
+var saveBtn14 = document.getElementById("saveBtn14");
+var saveBtn15 = document.getElementById("saveBtn15");
+var saveBtn16 = document.getElementById("saveBtn16");
+var saveBtn17 = document.getElementById("saveBtn17");
+
+var hour9 = document.getElementById("9");
+var hour10 = document.getElementById("10");
+var hour11 = document.getElementById("11");
+var hour12 = document.getElementById("12");
+var hour13 = document.getElementById("13");
+var hour14 = document.getElementById("14");
+var hour15 = document.getElementById("15");
+var hour16 = document.getElementById("16");
+var hour17 = document.getElementById("17");
 
 setInterval(dateTimeDisplay, 60000);
     
@@ -15,57 +29,99 @@ var dateTimeDisplay = function(){
     // console.log(currentDay);
     
     // add background color to hours depending on past, present and future
-        var now = moment().hour();
-        // console.log(now);
-        
-        var oneHourFuture = now + 1;
-        // console.log(oneHourFuture);
-        
+        var now = moment().hour();       
+        var oneHourFuture = now + 1;       
         var oneHourPast = now - 1;
-        // console.log(oneHourPast);
         
         $(".time").each(function(){
-            
-            // console.log(this.id);
 
             if (this.id >= oneHourFuture) {
-                // console.log(true);
                 $(this).addClass("future");
             
             } else if (this.id <= oneHourPast) {
-                // console.log(true);
                 $(this).addClass("past");
 
             } else if (this.id == now) {
-                // console.log(true);
                 $(this).addClass("present")
             }
         });        
 };
 
-// saveTasks function
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-};
-
 // loadTasks function
 var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    tasks9 = JSON.parse(localStorage.getItem("nineTasks"));
+    document.getElementById("9").innerHTML = tasks9;
+
+    tasks10 = JSON.parse(localStorage.getItem("tenTasks"));
+    document.getElementById("10").innerHTML = tasks10;
+   
+    tasks11 = JSON.parse(localStorage.getItem("elevenTasks"));
+    document.getElementById("11").innerHTML = tasks11;
+
+    tasks12 = JSON.parse(localStorage.getItem("twelveTasks"));
+    document.getElementById("12").innerHTML = tasks12;
+
+    tasks13 = JSON.parse(localStorage.getItem("oneTasks"));
+    document.getElementById("13").innerHTML = tasks13;
+
+    tasks14 = JSON.parse(localStorage.getItem("twoTasks"));
+    document.getElementById("14").innerHTML = tasks14;
+
+    tasks15 = JSON.parse(localStorage.getItem("threeTasks"));
+    document.getElementById("15").innerHTML = tasks15;
+
+    tasks16 = JSON.parse(localStorage.getItem("fourTasks"));
+    document.getElementById("16").innerHTML = tasks16;
+
+    tasks17 = JSON.parse(localStorage.getItem("fiveTasks"));
+    document.getElementById("17").innerHTML = tasks17;
 }
 
-$(".saveBtn").on("click", function(){
+// save tasks functions
+$(saveBtn9).on("click", function(){
     console.log("click");
-    $(hourlyTasks).each(function(){
-        tasks.push({
-            text:$(this)
-                .find("textarea")
-                .val()
-                .trim(),    
-    });
-    console.log(tasks);
-        saveTasks(tasks);
-    });
+    localStorage.setItem("nineTasks", JSON.stringify(hour9.value));
+});
+    
+$(saveBtn10).on("click", function(){
+    console.log("click");
+    localStorage.setItem("tenTasks", JSON.stringify(hour10.value));
+});
 
+$(saveBtn11).on("click", function(){
+    console.log("click");
+    localStorage.setItem("elevenTasks", JSON.stringify(hour11.value));
+});
+
+$(saveBtn12).on("click", function(){
+    console.log("click");
+    localStorage.setItem("twelveTasks", JSON.stringify(hour12.value));
+});
+
+$(saveBtn13).on("click", function(){
+    console.log("click");
+    localStorage.setItem("oneTasks", JSON.stringify(hour13.value));
+});
+
+$(saveBtn14).on("click", function(){
+    console.log("click");
+    localStorage.setItem("twoTasks", JSON.stringify(hour14.value));
+});
+
+$(saveBtn15).on("click", function(){
+    console.log("click");
+    localStorage.setItem("threeTasks", JSON.stringify(hour15.value));
+});
+
+$(saveBtn16).on("click", function(){
+    console.log("click");
+    localStorage.setItem("fourTasks", JSON.stringify(hour16.value));
+});
+
+$(saveBtn17).on("click", function(){
+    console.log("click");
+    localStorage.setItem("fiveTasks", JSON.stringify(hour17.value));
 });
 
 dateTimeDisplay();
